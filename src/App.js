3fc9ba3948cfe3b26bad68f4b1d6742d166b74a5
Carlos Chavez <hamandatorres@gmail.com>
2021-04-13 20:50:14 -0600

    finishing navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
 import routes from './routes';
-import './App.css';
+import './scss/main.scss'
+
+
 
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { mdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
 
 
 const Header = props => {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+  const handleToggle = () => {
+    setNavbarOpen(prev => !prev)
+  }
+  const closeMenu = () => {
+    setNavbarOpen(false)
+  }
   return (
-    <h1>Header</h1>
+  
+    <body>
+    <nav className='navbar'>
+    <div className='title'>Welcome</div>
+    <button href='#' className='toggle-button' onClick={handleToggle}>
+      {navbarOpen ? (
+        <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+        ) : (
+        <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+      )}
+      </button>
+    
+      <div className='navbar-links'>
+        <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+          <li>
+            <a href='#' 
+            to={'/'}
+            activeClassName="active-link"
+            onClick={() => closeMenu()}
+            exact >
+            Login</a>
+          </li>
+          <li>
+            <a href='#'
+             to={'/'}
+             activeClassName="active-link"
+             onClick={() => closeMenu()}
+             exact >
+              Register</a>
+              </li>
+          <li>
+            <a href='#'
+             to={'/topic'}
+             activeClassName="active-link"
+             onClick={() => closeMenu()}
+             exact >
+              Topics</a>
+              </li>
+        </ul>
+      </div>
+    </nav>
+    </body>
   )
 }
 
