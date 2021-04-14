0f2957e9fb83d0d735f844c2979ab3f153a31bfd
Carlos Chavez <hamandatorres@gmail.com>
2021-04-13 21:30:19 -0600

    halfway finished with nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,28 @@
 import React, { useState } from 'react';
-import { mdClose } from 'react-icons/md';
-import { FiMenu } from 'react-icons/fi';
 
 
 const Header = props => {
-  const [navbarOpen, setNavbarOpen] = useState(false)
-  const handleToggle = () => {
-    setNavbarOpen(prev => !prev)
-  }
-  const closeMenu = () => {
-    setNavbarOpen(false)
-  }
+
   return (
-  
     <body>
-    <nav className='navbar'>
-    <div className='title'>Welcome</div>
-    <button href='#' className='toggle-button' onClick={handleToggle}>
-      {navbarOpen ? (
-        <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
-        ) : (
-        <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
-      )}
-      </button>
-    
-      <div className='navbar-links'>
-        <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
-          <li>
-            <a href='#' 
-            to={'/'}
-            activeClassName="active-link"
-            onClick={() => closeMenu()}
-            exact >
-            Login</a>
-          </li>
-          <li>
-            <a href='#'
-             to={'/'}
-             activeClassName="active-link"
-             onClick={() => closeMenu()}
-             exact >
-              Register</a>
-              </li>
-          <li>
-            <a href='#'
-             to={'/topic'}
-             activeClassName="active-link"
-             onClick={() => closeMenu()}
-             exact >
-              Topics</a>
-              </li>
+    <nav class="navbar">
+      <div class="brand-title">RPG</div>
+      <a href="#" class="toggle-button">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </a>
+      <div class="navbar-links">
+        <ul>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Register</a></li>
+          <li><a href="#">Topics</a></li>
+          <li><a href="#">Logout</a></li>
+          <li><a href="#">Profile</a></li>
         </ul>
       </div>
     </nav>
-    </body>
+  </body>
   )
 }
-
 export default Header
