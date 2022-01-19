6ffb015d5bbbb5b3c545eb6dc1dd8e938fffea7a
Carlos A Chavez Perez <hamandatorres@gmail.com>
2022-01-18 23:06:27 -0700

    Deleted unneeded files

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import App from "./App";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-// import Amplify from "aws-amplify";
-// import config from './aws-exports';
-// Amplify.configure(config)
 
 const Router =
   process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
